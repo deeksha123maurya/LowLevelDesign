e66301367583aea3d9fb6a4ee3e0cbbca14034b7
deeksha123maurya <deeksha.mnnit18@gmail.com>
2024-09-20 22:40:52 +0530

    Low level Design Practice

---
+++ src/main/java/com/example/lowleveldesign/LoggingSystem/ErrorLogProcessor.java
@@ -0,0 +1,20 @@
+package com.example.lowleveldesign.LoggingSystem;
+
+public class ErrorLogProcessor extends LogProcessor{
+    ErrorLogProcessor(LogProcessor nexLogProcessor){
+        super(nexLogProcessor);
+    }
+
+    public void log(int logLevel,String message){
+
+        if(logLevel == ERROR) {
+            System.out.println("ERROR: " + message);
+        } else{
+
+            super.log(logLevel, message);
+        }
+
+    }
+
+
+}
